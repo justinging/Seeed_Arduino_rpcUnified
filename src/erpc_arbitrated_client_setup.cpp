6c779e4e6bb5cd8df7ac1c083913338eae3ee8a0
LynnL4 <lht856@foxmail.com>
2020-07-22 14:50:16 +0800

    add erpc arbitrated client which make client and sever with a samd transport.

--- src/erpc_arbitrated_client_setup.cpp
+++ src/erpc_arbitrated_client_setup.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2016, Freescale Semiconductor, Inc.
+ * Copyright (c) 2016, Freescale Semiconductor, Inc.
  * Copyright 2016-2017 NXP
  * All rights reserved.
  *
@@ -7,17 +7,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "erpc_client_setup.h"
+#include "erpc_arbitrated_client_setup.h"
+#include "erpc_arbitrated_client_manager.h"
 #include "erpc_basic_codec.h"
-#include "erpc_client_manager.h"
-#include "erpc_crc16.h"
 #include "erpc_manually_constructed.h"
 #include "erpc_message_buffer.h"
-#include "erpc_transport.h"
-#include <cassert>
+#include "erpc_transport_arbitrator.h"
 #if ERPC_NESTED_CALLS
 #include "erpc_threading.h"
 #endif
+#include <cassert>
 
 using namespace erpc;
 
@@ -26,34 +25,47 @@ using namespace erpc;
 ////////////////////////////////////////////////////////////////////////////////
 
 // global client variables
-static ManuallyConstructed<ClientManager> s_client;
+static ManuallyConstructed<ArbitratedClientManager> s_client;
 static ClientManager *g_client = NULL;
+
 static ManuallyConstructed<BasicCodecFactory> s_codecFactory;
+static ManuallyConstructed<TransportArbitrator> s_arbitrator;
+static ManuallyConstructed<BasicCodec> s_codec;
 static ManuallyConstructed<Crc16> s_crc16;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Code
 ////////////////////////////////////////////////////////////////////////////////
 
-void erpc_client_init(erpc_transport_t transport, erpc_mbf_t message_buffer_factory)
+erpc_transport_t erpc_arbitrated_client_init(erpc_transport_t transport, erpc_mbf_t message_buffer_factory)
 {
     assert(transport);
 
     // Init factories.
     s_codecFactory.construct();
 
-    // Init client manager with the provided transport.
-    s_client.construct();
+    // Create codec used by the arbitrator.
+    s_codec.construct();
+
+    // Init the arbitrator using the passed in transport.
+    s_arbitrator.construct();
     Transport *castedTransport = reinterpret_cast<Transport *>(transport);
     s_crc16.construct();
     castedTransport->setCrc16(s_crc16.get());
-    s_client->setTransport(castedTransport);
+    s_arbitrator->setSharedTransport(castedTransport);
+    s_arbitrator->setCodec(s_codec);
+
+    // Init the client manager.
+    s_client.construct();
+    s_client->setArbitrator(s_arbitrator);
     s_client->setCodecFactory(s_codecFactory);
     s_client->setMessageBufferFactory(reinterpret_cast<MessageBufferFactory *>(message_buffer_factory));
     g_client = s_client;
+
+    return reinterpret_cast<erpc_transport_t>(s_arbitrator.get());
 }
 
-void erpc_client_set_error_handler(client_error_handler_t error_handler)
+void erpc_arbitrated_client_set_error_handler(client_error_handler_t error_handler)
 {
     if (g_client != NULL)
     {
@@ -61,13 +73,13 @@ void erpc_client_set_error_handler(client_error_handler_t error_handler)
     }
 }
 
-void erpc_client_set_crc(uint32_t crcStart)
+void erpc_arbitrated_client_set_crc(uint32_t crcStart)
 {
     s_crc16->setCrcStart(crcStart);
 }
 
 #if ERPC_NESTED_CALLS
-void erpc_client_set_server(erpc_server_t server)
+void erpc_arbitrated_client_set_server(erpc_server_t server)
 {
     if (g_client != NULL)
     {
@@ -75,7 +87,7 @@ void erpc_client_set_server(erpc_server_t server)
     }
 }
 
-void erpc_client_set_server_thread_id(void *serverThreadId)
+void erpc_arbitrated_client_set_server_thread_id(void *serverThreadId)
 {
     if (g_client != NULL)
     {
@@ -85,7 +97,7 @@ void erpc_client_set_server_thread_id(void *serverThreadId)
 #endif
 
 #if ERPC_MESSAGE_LOGGING
-bool erpc_client_add_message_logger(erpc_transport_t transport)
+bool erpc_arbitrated_client_add_message_logger(erpc_transport_t transport)
 {
     if (g_client != NULL)
     {
@@ -95,10 +107,11 @@ bool erpc_client_add_message_logger(erpc_transport_t transport)
 }
 #endif
 
-void erpc_client_deinit(void)
+void erpc_arbitrated_client_deinit(void)
 {
-    s_crc16.destroy();
     s_client.destroy();
     s_codecFactory.destroy();
+    s_codec.destroy();
+    s_arbitrator.destroy();
     g_client = NULL;
 }
