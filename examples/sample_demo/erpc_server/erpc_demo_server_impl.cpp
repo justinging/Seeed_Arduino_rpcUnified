1a6bbfd037d6a8573423a407b3484b54e4b69620
LynnL4 <lht856@foxmail.com>
2020-07-22 16:00:45 +0800

    remove useless code in simple demo erpc_server.

--- examples/sample_demo/erpc_server/erpc_demo_server_impl.cpp
+++ examples/sample_demo/erpc_server/erpc_demo_server_impl.cpp
@@ -38,13 +38,11 @@ lockErrors_t RD_demoHello2(const binary_t *txInput, binary_t *txOutput)
     auto msg = "hi!";
     auto length = strlen(msg) + 1;
 
-    binary_t *result = NULL;
     char *buf = NULL;
 
     buf = (char *) erpc_malloc(length * sizeof(char));
     strncpy(buf, msg, length);
 
-    result = (binary_t *) erpc_malloc(sizeof(binary_t));
 	txOutput->dataLength = length;
 	txOutput->data = (uint8_t *)buf;
 	return lErrorOk_c;
@@ -56,13 +54,11 @@ lockErrors_t RD_demoHello3(const binary_t *txInput, int8_t txOutput[64], int32_t
     auto msg = "hi!";
     auto length = strlen(msg) + 1;
 
-    binary_t *result = NULL;
     char *buf = NULL;
 
     buf = (char *) erpc_malloc(length * sizeof(char));
     strncpy(buf, msg, length);
 
-    result = (binary_t *) erpc_malloc(sizeof(binary_t));
 	*size = length;
 	strncpy((char*)txOutput, buf, 64);
 	return lErrorOk_c;
